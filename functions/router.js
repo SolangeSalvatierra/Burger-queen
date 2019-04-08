7d908e5b95f8ee9a3d6646a9389a77f7d16c49a2
Tu-username <tucorreo@gmail.com>
2019-04-08 10:21:44 -0500

    templates completo, header y parte del CSS responsive

--- functions/router.js
+++ functions/router.js
@@ -12,8 +12,8 @@ const changeTmp = (hash) => {
 
 const viewTmp = (routers) => {
     const router = routers.substr(2, routers.length -2)
-    const container = document.getElementById("container")
-    container.innerHTML = objTemp[router];
+    const contenedor = document.getElementById("contenedor")
+    contenedor.innerHTML = objTemp[router];
 }
 window.addEventListener('load', changeTmp(window.location.hash))
 if (("onhashchange" in window)) window.onhashchange = () => changeTmp(window.location.hash)
--- functions/templates/tempString.js
+++ functions/templates/tempString.js
@@ -1,22 +1,72 @@
 export const objTemp = {
     home : `<div class="conteiner">
     <h1 class="text-center"> Burger Queen </h1>
+    <p> s./15 ¡Oferta! </p>
+    <ul class ="slider">
+        <li>
+            <input type="radio" id="sbutton1" name = "imagen1" checked>
+            <label for =""> </label>
+            <img src="img/ima1.jpg" alt="">
+        </li>    
+        <li>
+            <input type="radio" id="sbutton2" name = "imagen2">
+            <label for =""> </label>
+            <img src="img/ima2.jpg" alt="">
+        </li>
+        <li>
+            <input type="radio" id="sbutton3" name = "imagen3">
+            <label for =""> </label>
+            <img src="img/ima3.jpg" alt="">
+        </li> 
+        <li>
+            <input type="radio" id="sbutton4" name = "imagen4">
+            <label for =""> </label>
+            <img src="img/ima4.jpg" alt="">
+        </li>
+    </ul>
+
+    
+
+    <button id="orden" class="btn"> Pedido </button> 
     
 </div>`,
 
     desayuno : `<div class="conteiner">
-    <h2 class="text-center"> MENÚ </h2>
-    <h3> Desayuno </h3>
-    <p> Café americano	
-    Café con leche	
-    Sandwich de jamón y queso	
-    Jugo de frutas natural </p >
+    <h1 class="text-center"> MENÚ </h1>
+    <h3> Desayunos </h3>
+    <p> Café americano	-------- s/.5
+    <br> Café con leche	 -------- s/.7
+    <br> Sandwich de jamón y queso	-------- s./10
+    <br> Jugo de frutas natural     -------- s/.7
+    </p >
+    <button id="pedido" class="btn-orden"> Pedido </button> 
+
+
 </div>`,
 
     menu: `<div class="conteiner">
-    <h1 class="text-center"> Menu </h1>
-    <figure>
-    </figure>
+    <h1 class="text-center"> MENÚ </h1>
+    <h2> Almuerzos </h2>
+    <h4> HAMBUERGUESAS </h4>
+    <p> Hambuerguesas simples -------- s/.10
+    <br> Hamburguesas doble -------- s/.15
+    </p>
+    <h4> EXTRAS </h4>
+    <p> Huevo -------- + s/.1
+    <br> Queso  -------- + s/.1
+    </p>
+    <h4> ACOMPAÑANTES </h4>
+    <p> Papas fritas -------- s/.5
+    <br> Aros de cebolla -------- s/.5 
+    </p>
+    <h4> BEBIDAS </h4>
+    <p> Agua 500 ml -------- s/.5 
+    <br> Agua 750ml -------- s/.7
+    <br> Gaseosa 500ml -------- s/.7
+    <br> Gaseosa 750ml -------- s/.10
+    </p>
+    <button id="pedidos" class="btn-orden"> Pedido </button> 
+
 </div>`,
 
     different: `<div id="message">
