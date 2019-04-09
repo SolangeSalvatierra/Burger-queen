ed4664e32a01e8efd0cb956b4bddff5cbce9353d
SolangeCorySalvatierraQuispe <solangraf@gmail.com>
2019-04-08 19:17:42 -0500

    CCS y responsive

--- functions/templates/tempString.js
+++ functions/templates/tempString.js
@@ -1,6 +1,6 @@
 export const objTemp = {
     home : `<div class="conteiner">
-    <h1 class="text-center"> Burger Queen </h1>
+    <h2 class="text-center"> Burger Queen </h2>
     <p> s./15 ¡Oferta! </p>
     <ul class ="slider">
         <li>
@@ -32,12 +32,11 @@ export const objTemp = {
 </div>`,
 
     desayuno : `<div class="conteiner">
-    <h1 class="text-center"> MENÚ </h1>
-    <h3> Desayunos </h3>
-    <p> Café americano	-------- s/.5
-    <br> Café con leche	 -------- s/.7
-    <br> Sandwich de jamón y queso	-------- s./10
-    <br> Jugo de frutas natural     -------- s/.7
+    <h2 class="text-center"> MENU </h2>  <h3> Desayunos </h3> <br>
+    <p> Café americano	----- s/.5 <br>
+        Café con leche	 -----s/.7 <br>
+        Sandwich de jamón y queso	-----s./10 <br>
+        Jugo de frutas natural     -----s/.7 <br>
     </p >
     <button id="pedido" class="btn-orden"> Pedido </button> 
 
@@ -45,19 +44,18 @@ export const objTemp = {
 </div>`,
 
     menu: `<div class="conteiner">
-    <h1 class="text-center"> MENÚ </h1>
-    <h2> Almuerzos </h2>
+    <h2 class="text-center"> MENU </h2> <h3> Almuerzos </h3>
     <h4> HAMBUERGUESAS </h4>
-    <p> Hambuerguesas simples -------- s/.10
-    <br> Hamburguesas doble -------- s/.15
+    <p> Hambuerguesas simples -----s/.10
+    <br> Hamburguesas doble -----s/.15
     </p>
     <h4> EXTRAS </h4>
-    <p> Huevo -------- + s/.1
-    <br> Queso  -------- + s/.1
+    <p> Huevo -----+ s/.1
+    <br> Queso  -----+ s/.1
     </p>
-    <h4> ACOMPAÑANTES </h4>
-    <p> Papas fritas -------- s/.5
-    <br> Aros de cebolla -------- s/.5 
+    <h> ACOMPAÑANTES </h4>
+    <p> Papas fritas -----s/.5
+    <br> Aros de cebolla -----s/.5 
     </p>
     <h4> BEBIDAS </h4>
     <p> Agua 500 ml -------- s/.5 
