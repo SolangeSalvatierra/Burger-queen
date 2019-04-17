524590990f0bc047ecafec415d5533162604b404
SolangeCorySalvatierraQuispe <solangraf@gmail.com>
2019-04-17 17:39:41 -0500

    mostrando data de farebase en template

---
+++ src/controller/controller-firebase.js
@@ -0,0 +1,14 @@
+
+
+
+  
+  
+//   // Initialize Cloud Firestore through Firebase
+//   var db = firebase.firestore();
+//   //leer documentos
+//   db.collection("desayuno").get().then((querySnapshot) => {
+//     querySnapshot.forEach((doc) => {
+//         console.log(`${doc.id} => ${doc.data().pedido}`);
+//     });
+// });
+
--- src/templates/desayuno.js
+++ src/templates/desayuno.js
@@ -5,33 +5,9 @@ export const desayuno = () => {
         <h2 class="inline"> MENU </h2> 
         <h3 class="inline"> Desayunos </h3> <br>
         <input class="text-field" id="cliente" type="text">
-        <div class="conteiner">
-        
-        <div id ="pedido1" class= "inline" > Café americano -----  </div> 
-        <div id="precio1" class= "inline" > s/.5  </div> 
-        <img src="img/flecha-02.png" alt="" id="cafe-ame">      
-        <div class= "inline" > <input class="text" id="cantidad" type="text"> 
-        <input class="text" id="precios" type="text"> </div>              
-        <br>
-        <div id="pedido2" class= "inline" > Café con leche ----- </div> 
-        <div id ="precio2" class= "inline" > s/.7 </div>  
-        <img src="img/flecha-02.png" alt=""  id="cafe-lech"> 
-        <div class= "inline"> <input class="text" id="cantidad" type="text"> 
-        <input class="text" id="precios" type="text"> </div>
-        <br>
-        <div id="pedido3" class= "inline" > Sandwich de jamón y queso ----- </div> 
-        <div id="precio3" class= "inline" > s./10 </div>
-        <img src="img/flecha-02.png" alt=""  id="sand-jam"> 
-        <div class= "inline"> <input class="text" id="cantidad" type="text"> 
-        <input class="text" id="precios" type="text"> </div>
-        <br>
-        <div id="pedido4" class= "inline"> Jugo de frutas natural ----- </div>
-        <div id="precio4" class= "inline"> s/.7 </div> 
-        <img src="img/flecha-02.png" alt=""  id="jugo"> 
-        <div class= "inline"> <input class="text" id="cantidad" type="text"> 
-        <input class="text" id="precios" type="text"> </div>
-        <br>
-        </div>
+        <div id="menu" class="conteiner">
+
+
         <button id="pedido" class="btn"> Pedido </button>
     </div>
     `;
@@ -40,12 +16,6 @@ export const desayuno = () => {
     const nombre = sessionStorage.getItem("Nombre");
     /*Mostrar datos almacenados*/
     breakfast.querySelector("#cliente").value = nombre;
-
-
-
-
-
-    
     return breakfast
     };
 
